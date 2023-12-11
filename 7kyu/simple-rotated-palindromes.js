fd967580f04d0f1daee0fb8d716924e9f0521842
Domenic Polsoni <domenic.polsoni@gmail.com>
2023-12-11 16:18:08 -0500

    adding simple rotated palindromes

---
+++ 7kyu/simple-rotated-palindromes.js
@@ -0,0 +1,32 @@
+// DESCRIPTION:
+// In this Kata, you will be given a string and your task is to determine if that string can be a palindrome if we rotate one or more characters to the left.
+
+// solve("4455") = true, because after 1 rotation, we get "5445" which is a palindrome
+// solve("zazcbaabc") = true, because after 3 rotations, we get "abczazcba", a palindrome
+// More examples in test cases. Input will be strings of lowercase letters or numbers only.
+//
+// My solution
+
+function solve(st)
+{
+    function isPalindrom(s)
+    {
+        for (let i = 0, j = s.length; i < j; i++)
+        {
+            if (s.charAt(i) !== s.charAt(j - i - 1))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    for (let i = 0, j = st.length; i < j; i++)
+    {
+        st = st.charAt(st.length - 1) + st.substring(0, st.length - 1);
+        if (isPalindrom(st))
+        {
+            return true;
+        }
+    }
+    return false;
+}
